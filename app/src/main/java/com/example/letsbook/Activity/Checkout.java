b302af0ee61fe53cd7497b135cfcb54e7c94bfc1
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2023-10-08 09:32:42 +0530

    addedDifferentLayouts

---
+++ app/src/main/java/com/example/letsbook/Activity/Checkout.java
@@ -0,0 +1,16 @@
+package com.example.letsbook.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.letsbook.R;
+
+public class Checkout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout);
+    }
+}
