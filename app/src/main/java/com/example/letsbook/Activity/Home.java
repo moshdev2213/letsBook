d1eb67953f064c523d07ca7c438985420bc7a95c
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2023-10-08 06:30:42 +0530

    addedSplashScreenInit

---
+++ app/src/main/java/com/example/letsbook/Activity/Home.java
@@ -0,0 +1,18 @@
+package com.example.letsbook.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
+
+import android.os.Bundle;
+
+import com.example.letsbook.R;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_home);
+    }
+}
--- app/src/main/java/com/example/letsbook/Activity/SignIn.java
+++ app/src/main/java/com/example/letsbook/Activity/SignIn.java
@@ -1,7 +1,9 @@
 package com.example.letsbook.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import com.example.letsbook.R;
@@ -11,6 +13,15 @@ public class SignIn extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            getSplashScreen();
+        }
         setContentView(R.layout.activity_sign_in);
     }
 }
