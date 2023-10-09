aed76dbe94b8041365212c74e614fcc58fc6bb5f
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2023-10-09 10:50:15 +0530

    addedValidationLogin

--- app/src/main/java/com/example/letsbook/Activity/SignIn.java
+++ app/src/main/java/com/example/letsbook/Activity/SignIn.java
@@ -1,15 +1,25 @@
 package com.example.letsbook.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.splashscreen.SplashScreen;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.example.letsbook.FormData.SignInForm;
 import com.example.letsbook.R;
+import com.example.letsbook.Validation.ValidationResult;
 
 public class SignIn extends AppCompatActivity {
-
+    private EditText etSignInEmail;
+    private EditText etSignInPassword;
+    private CardView cvSignInBtn;
+    private TextView tvredirectToSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +33,47 @@ public class SignIn extends AppCompatActivity {
             getSplashScreen();
         }
         setContentView(R.layout.activity_sign_in);
+
+        etSignInPassword = findViewById(R.id.etSignInPassword);
+        etSignInEmail = findViewById(R.id.etSignInEmail);
+        tvredirectToSignUp = findViewById(R.id.tvredirectToSignUp);
+        cvSignInBtn = findViewById(R.id.cvSignInBtn);
+
+        cvSignInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginValidator(etSignInEmail.getText().toString(),etSignInPassword.getText().toString());
+            }
+        });
+        tvredirectToSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), SignUp.class));
+            }
+        });
+
     }
+    private void loginValidator(String email, String password) {
+        SignInForm userLoginForm = new SignInForm(email, password);
+        ValidationResult emailValidation = userLoginForm.validateEmail(email);
+        ValidationResult passwordValidation = userLoginForm.validatePassword(password);
+        int count = 0; // Assuming you have declared and initialized 'count' earlier
+
+        if (emailValidation instanceof ValidationResult.Valid) {
+            count++;
+        } else if (emailValidation instanceof ValidationResult.Invalid) {
+            etSignInEmail.setError(((ValidationResult.Invalid) emailValidation).getErrorMsg());
+        } else if (emailValidation instanceof ValidationResult.Empty) {
+            etSignInEmail.setError(((ValidationResult.Empty) emailValidation).getErrorMsg());
+        }
+
+        if (passwordValidation instanceof ValidationResult.Valid) {
+            count++;
+        } else if (passwordValidation instanceof ValidationResult.Invalid) {
+            etSignInPassword.setError(((ValidationResult.Invalid) passwordValidation).getErrorMsg());
+        } else if (passwordValidation instanceof ValidationResult.Empty) {
+            etSignInPassword.setError(((ValidationResult.Empty) passwordValidation).getErrorMsg());
+        }
+    }
+
 }
--- app/src/main/java/com/example/letsbook/FormData/SignInForm.java
+++ app/src/main/java/com/example/letsbook/FormData/SignInForm.java
@@ -4,9 +4,12 @@ import com.example.letsbook.Validation.ValidationResult;
 
 public class SignInForm {
     private String tel;
+    private String email;
+    private String password;
 
-    public SignInForm(String tel) {
-        this.tel = tel;
+    public SignInForm(String email,String password) {
+        this.email = email;
+        this.password = password;
     }
 
     public ValidationResult validateTel() {
@@ -20,4 +23,30 @@ public class SignInForm {
             return ValidationResult.Valid.getInstance();
         }
     }
+    public ValidationResult validatePassword(String password) {
+        // The regex pattern
+        String passwordPattern = "^(?=.*[a-zA-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$";
+
+        if (password.isEmpty()) {
+            return new ValidationResult.Empty("Enter Password");
+        } else if (!password.matches(passwordPattern)) {
+            return new ValidationResult.Invalid("Invalid ex: Aa@asda22");
+        } else {
+            return ValidationResult.Valid.getInstance();
+        }
+    }
+
+    public ValidationResult validateEmail(String email) {
+        // The regex pattern
+        String regex = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}";
+
+        if (email.isEmpty()) {
+            return new ValidationResult.Empty("Enter Email");
+        } else if (!email.matches(regex)) {
+            return new ValidationResult.Invalid("Enter Valid Email");
+        } else {
+            return ValidationResult.Valid.getInstance();
+        }
+    }
+
 }
